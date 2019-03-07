d43e83de5aeab2d79944e58be7a1c473b30ae902
‘RSpidla’ <rspidla@gmail.com>
2019-03-07 05:23:48 +0000

    Create recursive function to sum arrays

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let element = 0;
+  if(array.length > 0){
+    element = array.shift();
+    if(Array.isArray(element)){
+      element = sumItems(element);
+    }
+  } else {
+    return 0;
+  }
+  return element + sumItems(array);
 }
 
 module.exports = sumItems;
